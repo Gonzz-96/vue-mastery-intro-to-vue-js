fa72d4c8783ac0f22fbf04851448a015848467a4
Gonzalo Campos <erick.campos@wizeline.com>
2020-06-27 17:22:49 -0500

    Add style and class binding.

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ var app = new Vue({
     product: 'Socks',
     image: './assets/vmSocks-green-onWhite.jpg',
     inventory: 100,
+    inStock: false,
     details: ['80% cotton', '20% polyester', 'Gender-neutral'],
     variants: [
       {
