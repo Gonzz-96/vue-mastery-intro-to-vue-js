50e4ea556c88c72807ecf9c9a98bb7c219576d3f
Gonzalo Campos <erick.campos@wizeline.com>
2020-06-27 20:22:47 -0500

    Add cmmonucation between compent and its parent.

--- main.js
+++ main.js
@@ -35,10 +35,6 @@ Vue.component('product', {
       <button v-on:click="addToCart" 
               :disabled="!inStock"
               :class="{ disabledButton: !inStock }">Add to cart</button>
-
-      <div class="cart">
-        <p>Cart({{ cart }})</p>
-      </div>
     </div>
   </div>
   `,
@@ -62,13 +58,12 @@ Vue.component('product', {
           variantImage: './assets/vmSocks-blue-onWhite.jpg',
           variantQuantity: 0
         }
-      ],
-      cart: 0
+      ]
     }
   },
   methods: {
     addToCart() {
-      this.cart += 1;
+      this.$emit('add-to-cart', this.variants[this.selectedVariant].variantId);
     },
     updateProduct(index) {
       this.selectedVariant = index;
@@ -97,6 +92,12 @@ Vue.component('product', {
 var app = new Vue({
   el: '#app',
   data: {
-    premium: true
+    premium: true,
+    cart: []
+  },
+  methods: {
+    updateCart(id) {
+      this.cart.push(id);
+    }
   }
 })
